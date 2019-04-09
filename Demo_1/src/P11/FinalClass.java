1de8abfa654a12ab1a7c36f201daf1833d4c957c
ledehui <ledehui@FSOFFICE137.FSTVOFFICE.CN>
2019-04-09 12:09:26 +0800

    20190409-001

---
+++ Demo_1/src/P11/FinalClass.java
@@ -0,0 +1,26 @@
+package P11;
+
+final class FinalClass {
+	int a = 3;
+
+	void doit() {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinalClass f = new FinalClass();
+		f.a++;
+		System.out.println(f.a);
+		f.a++;
+		System.out.println(f.a);
+		abcd aAbcd = new abcd();
+		System.out.println(aAbcd.a);
+	}
+	
+
+}
+
+class abcd{
+	int a=3;
+	
+}
---
+++ Demo_1/src/P11/FinalMethod.java
@@ -0,0 +1,47 @@
+package P11;
+
+class Parents  {
+	// TODO Auto-generated constructor stub
+	
+	private final void doit() {
+		System.out.println("父类doit()");
+		
+	}
+	final void doit2() {
+		System.out.println("父类doit2()");
+	}
+	public void doit3() {
+		System.out.println("父类doit3()");
+		
+	}
+}
+
+class Sub extends Parents{
+	public final void doit() {
+		System.out.print("子类doit()");
+	}
+//	final void doit2() {
+//		System.out.println("子类doit2()");
+//	}
+	public void doit3() {
+		System.out.print("子类doit3()");
+		
+	}
+}
+
+
+
+public class FinalMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sub sub = new Sub();
+		sub.doit(); 
+		Parents parents = sub;
+		
+		parents.doit2();
+		parents.doit3();
+
+	}
+
+}
