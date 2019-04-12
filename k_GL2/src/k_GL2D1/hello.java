25cb95c2d1567f2402094ef0807b084aabef95b7
ledehui <ledehui@FSOFFICE137.FSTVOFFICE.CN>
2019-04-12 08:36:26 +0800

    20190412TEST1
    Signed-off-by: ledehui <ledehui@FSOFFICE137.FSTVOFFICE.CN>

--- k_GL2/src/k_GL2D1/hello.java
+++ k_GL2/src/k_GL2D1/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello01100");
+		System.out.println("hello011002222");
 	}
 
 }
