a92244f0119bdec1575ca6806e34e0e4f5c343ba
LeDeHui <18379233301@163.com>
2019-05-13 13:59:30 +0800

    2019-5-13 13:59:22

--- K_GL_JDBC/src/com/JDBC/Day_01/TestJDBC.java
+++ K_GL_JDBC/src/com/JDBC/Day_01/TestJDBC.java
@@ -1,11 +1,14 @@
-package Day_01;
+package com.JDBC.Day_01;
 
 import org.junit.Test;
 
 public class TestJDBC {
 	/*
-	 * Driver 是一个接口:数据库厂商必须提供实现的接口,能从其中获取数据库连接
+	 * Driver 是一个接口 
+	 * 	数据库厂商必须提供实现的接口,能从其中获取数据库连接
 	 * 
+	 * 
+	 * 1.加入驱动
 	 * 
 	 * */
 	@Test
