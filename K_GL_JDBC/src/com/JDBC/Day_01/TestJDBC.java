b58a62c7516203404d55a7d08cd77670d79e811e
LeDeHui <18379233301@163.com>
2019-05-14 12:44:40 +0800

    2019-5-14 12:44:38

--- K_GL_JDBC/src/com/JDBC/Day_01/TestJDBC.java
+++ K_GL_JDBC/src/com/JDBC/Day_01/TestJDBC.java
@@ -1,25 +1,60 @@
 package com.JDBC.Day_01;
- 
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.Date;
 import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Properties;
 
 import org.junit.Test;
- 
- 
 
 public class TestJDBC {
 	/*
-	 * Driver 是一个接口 
-	 * 	数据库厂商必须提供实现的接口,能从其中获取数据库连接
-	 * 
-	 * 
-	 * 1.加入驱动
-	 * 
-	 * */
+	 * Driver 是一个接口 数据库厂商必须提供实现的接口,能从其中获取数据库连接
+	 */
 	@Test
-	public void testDriver() { 
-		 
-		
+	public void testDriver() {
+		// 1. 创建一个 Driver 实现类的对象
+		/*
+		 * Driver driver = new com.mysql.jdbc.Driver();
+		 * 
+		 * // 2. 准备连接数据库的基本信息: url, user, password String url =
+		 * "jdbc:mysql://localhost:3306/test"; Properties info = new Properties();
+		 * info.put("user", "root"); info.put("password", "1230");
+		 * 
+		 * // 3. 调用 Driver 接口的 connect(url, info) 获取数据库连接 Connection connection =
+		 * (Connection) driver.connect(url, info); System.out.println(connection);
+		 */
 	}
-	
+
+	public Connection getConnection() throws Exception {
+		String driverClass = null;
+		String jdbcUrl = null;
+		String user = null;
+		String password = null;
+		InputStream is = getClass().getClassLoader().getResourceAsStream("jdbc.properties");
+		Properties properties = new Properties();
+		properties.load(is);
+		driverClass = properties.getProperty("dirver");
+		jdbcUrl = properties.getProperty("jdbcUrl");
+		user = properties.getProperty("user");
+		password = properties.getProperty("password");
+		Driver driver = (Driver) Class.forName(driverClass).newInstance();
+		Properties info = new Properties();
+		info.put("user", user);
+		info.put("password", password);
+		Connection connection = driver.connect(jdbcUrl, info);
+		return connection;
+	}
+
+	@Test
+	public void testGetConnection() throws Exception {
+		System.out.println(getConnection());
+	}
 
 }
--- K_GL_JDBC/src/com/JDBC/Day_01/TestJDBC2_LDH.java
+++ K_GL_JDBC/src/com/JDBC/Day_01/TestJDBC2_LDH.java
@@ -8,13 +8,7 @@ import java.sql.SQLException;
 import org.junit.Test;
 
 public class TestJDBC2_LDH {
-	/*
-	 * Driver 是一个接口 数据库厂商必须提供实现的接口,能从其中获取数据库连接
-	 * 
-	 * 
-	 * 1.加入驱动
-	 * 
-	 */
+	 
 	@Test
 	public void testDriver() {
 		Connection conn = null;// 创建一个数据库连接
@@ -22,16 +16,21 @@ public class TestJDBC2_LDH {
 		ResultSet resultSet = null;// 创建一个结果集对象
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");// 加载Oracle驱动程序,不用DriverManager
-			String url = "jdbc:oracle:thin:@127.0.0.1:1521:Oracle";// Oracle地址
-			String user = "scott";// 数据库用户名
-			String password = "tiger";// 数据库密码
+			String url = "jdbc:oracle:thin:@127.0.0.1:1158:Oracle";// Oracle地址
+			String user = "system";// 数据库用户名
+			String password = "Ledehui1";// 数据库密码
 			conn = DriverManager.getConnection(url, user, password);// 获取连接
-			String sql = "select * from EMP where ename=?";// 预编译语句，“？”代表参数
+			//String sql = "select * from EMP where ename=?";// 预编译语句，“？”代表参数
+			String sql = "select  *  from OFFICE_TIME t  where YYYY = ? and  MM=? and DD=?";
 			ps = conn.prepareStatement(sql);// 实例化预编译语句
-			ps.setString(1, "SMITH");// 设置参数，前面的1表示第一个问号（第二个问号就用2）
+			//ps.setString(1, "SMITH");// 设置参数，前面的1表示第一个问号（第二个问号就用2）
+			ps.setString(1, "2019");
+			ps.setString(2, "5");
+			ps.setString(3, "9");
 			resultSet = ps.executeQuery();// 执行查询
 			while (resultSet.next()) {// 当结果集不为空时
-				System.out.println(resultSet.getString("MGR"));
+				//System.out.println(resultSet.getString("MGR"));
+				System.out.println(resultSet.getString("TIME_ADD"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
