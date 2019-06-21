338c629120b350d743b334e9ba69c5bb9c34fa8d
LeDeHui <18379233301@163.com>
2019-06-21 14:16:26 +0800

    2019-6-21 14:16:25

--- ALiYunDemo/src/Demo1/Hello.java
+++ ALiYunDemo/src/Demo1/Hello.java
@@ -3,20 +3,88 @@ package Demo1;
 public class Hello {
 
 	public static void main(String[] args) {
-		String str = "hello" ;
-		switch (str) {
-			case "Hello":{
-				System.out.println("Hello") ;
-				break ;
-			}
-			case "hello": {
-				System.out.println("hello") ;
-				break ;
-			}
-			default: {
-				System.out.println("NoMatch") ;
-			}
+		
+		int i = 1;
+		int j = i++;
+		if ((i ==(++j)&&(i++)==j)) {
+			i +=j;
 		}
+		System.out.println("i = " + i );
+		int num = 2147483647;
+		num +=2;
+		System.out.println(num);
+		
+//		System.out.println(sum(90)) ;
+//		}
+//		public static double sum(int num) {
+//			if (num == 1) {
+//				return 1 ;
+//			}
+//			return fan(num) + sum(num - 1) ;
+//		}
+//		public static double fan(int num) {	// 执行累加
+//			if (num == 1) {	// 不累加了
+//				return 1 ;
+//			}
+//			return num * fan(num - 1) ;	// 递归调用
+		
+//		System.out.println(sum(100)) ;
+//		}
+//		public static int sum(int num) {	// 执行累加
+//			if (num == 1) {	// 不累加了
+//				return 1 ;
+//			}
+//			return num + sum(num - 1) ;	// 递归调用
+		 
+//		for (int x = 1 ; x <= 9 ; x  ++) {	// 口诀表最多9行
+//			for (int y = 1 ; y <= x ; y ++) {
+//				System.out.print(y + "*" + x + "=" + (x * y) + "\t") ;
+//			}
+//			System.out.println() ;// 换行
+//		}
+		
+//		int line = 5 ; // 总体行数
+//		for (int x = 0 ; x < line ; x ++) {
+//			for (int y = 0 ; y < line - x ; y ++) {
+//				System.out.print(" ") ;
+//			}
+//			for (int y = 0 ; y <= x ; y ++) {
+//				System.out.print("* ") ;
+//			}
+//			System.out.println() ;
+//		}
+		
+//		int sum = 0 ;	// 保存最终的计算总和
+//		int num = 1 ; // 进行循环控制
+//		do {	// 循环的执行条件
+//			sum += num ;	// 累加
+//			num ++ ;	// 修改循环条件
+//		} while (num <= 100) ;
+//		System.out.println(sum) ;
+		
+//		int sum = 0 ;	// 保存最终的计算总和
+//		int num = 1 ; // 进行循环控制
+//		while (num <= 1 ) {	// 循环的执行条件
+//			sum += num ;	// 累加
+//			num ++ ;	// 修改循环条件
+//		}
+//		System.out.println(sum) ;
+		
+//		String str = "hello" ;
+//		switch (str) {
+//			case "Hello":{
+//				System.out.println("Hello") ;
+//				break ;
+//			}
+//			case "hello": {
+//				System.out.println("hello") ;
+//				break ;
+//			}
+//			default: {
+//				System.out.println("NoMatch") ;
+//			}
+//		}
+		
 		// TODO Auto-generated method stub
 		//System.out.println("10");
 		/*int a = 12;
