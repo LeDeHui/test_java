867b531593bf89e939f2c76998c4fcb45cc3872e
LeDeHui <18379233301@163.com>
2019-06-21 11:18:40 +0800

    2019-6-21 11:18:39

--- ALiYunDemo/src/Demo1/Hello.java
+++ ALiYunDemo/src/Demo1/Hello.java
@@ -3,8 +3,46 @@ package Demo1;
 public class Hello {
 
 	public static void main(String[] args) {
+		String str = "hello" ;
+		switch (str) {
+			case "Hello":{
+				System.out.println("Hello") ;
+				break ;
+			}
+			case "hello": {
+				System.out.println("hello") ;
+				break ;
+			}
+			default: {
+				System.out.println("NoMatch") ;
+			}
+		}
 		// TODO Auto-generated method stub
-		System.out.println("10");
+		//System.out.println("10");
+		/*int a = 12;
+		int b = 15;
+		a= a^b;
+		b= a^b;
+		a= a^b;
+		System.out.println("a:" + a);
+		System.out.println("b:" + b);
+		Hello1 hello1 = new Hello1();
+		hello1.name();*/
+		/*
+		int ch = 2 ;
+		switch (ch) {
+			case 2 :
+				System.out.println("设置的内容是2。") ;
+				break ;
+			case 1 : {
+				System.out.println("设置的内容是1。") ;
+				break ;
+			}
+			default : {
+				System.out.println("没有内容满足。") ;
+				break ;
+			}
+		}*/
 	}
 
 }
---
+++ ALiYunDemo/src/Demo1/Hello1.java
@@ -0,0 +1,13 @@
+package Demo1;
+
+class Hello1 {
+	 
+		// TODO Auto-generated method stub
+		//System.out.println("10");
+		 
+	public void name() {
+		System.out.println("a");
+	}
+	 
+
+}
