512333cb82c4226a9f49dcb015835463eb350a80
LeDeHui <18379233301@163.com>
2019-06-21 16:35:30 +0800

    2019-6-21 16:35:28

--- ALiYunDemo/src/Demo1/Hello.java
+++ ALiYunDemo/src/Demo1/Hello.java
@@ -87,30 +87,32 @@ public class Hello {
 		
 		// TODO Auto-generated method stub
 		//System.out.println("10");
-		/*int a = 12;
-		int b = 15;
-		a= a^b;
-		b= a^b;
-		a= a^b;
-		System.out.println("a:" + a);
-		System.out.println("b:" + b);
-		Hello1 hello1 = new Hello1();
-		hello1.name();*/
-		/*
-		int ch = 2 ;
-		switch (ch) {
-			case 2 :
-				System.out.println("设置的内容是2。") ;
-				break ;
-			case 1 : {
-				System.out.println("设置的内容是1。") ;
-				break ;
-			}
-			default : {
-				System.out.println("没有内容满足。") ;
-				break ;
-			}
-		}*/
+		
+//		int a = 12;
+//		int b = 15;
+//		a= a^b;
+//		b= a^b;
+//		a= a^b;
+//		System.out.println("a:" + a);
+//		System.out.println("b:" + b);
+//		Hello1 hello1 = new Hello1();
+//		hello1.name();
+		
+		 
+//		int ch = 2 ;
+//		switch (ch) {
+//			case 2 :
+//				System.out.println("设置的内容是2。") ;
+//				break ;
+//			case 1 : {
+//				System.out.println("设置的内容是1。") ;
+//				break ;
+//			}
+//			default : {
+//				System.out.println("没有内容满足。") ;
+//				break ;
+//			}
+//		} 
 	}
 
 }
