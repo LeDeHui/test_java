4ee5930bcf98e729301f7ad271910ccb0716bb90
LeDeHui <18379233301@163.com>
2019-07-04 14:57:56 +0800

    2019-7-4 14:57:55

--- Demo_1/src/H10/list42/list_42_1.java
+++ Demo_1/src/H10/list42/list_42_1.java
@@ -6,7 +6,9 @@ public class list_42_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String str = "mldn";
+		String str1 = new String("MLDN");
+		
 	}
 
 }
